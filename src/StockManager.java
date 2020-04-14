5e84e4dfd8faa7e69d1d4747e681bb388532eacd
82102 <82102@DESKTOP-55UIN0I>
2020-04-14 22:15:56 +0900

    modified the edit menu

--- src/StockManager.java
+++ src/StockManager.java
@@ -40,8 +40,36 @@ public class StockManager {
 		System.out.print("Stock Number:");
 		int stocknumber = input.nextInt();
 		if (stock.number == stocknumber ) {
-			stock = null;
-			System.out.println("the stock to be edited" + stocknumber);
+			int num = -1;
+			while (num != 5) {
+				System.out.println("** Stock Info Edit Menu **");
+				System.out.println(" 1. Edit Number");
+				System.out.println(" 2. Edit Name"); 
+				System.out.println(" 3. Edit Manufacturing Date"); 
+				System.out.println(" 4. Edit Expiration Date");  
+				System.out.println(" 5. Exit"); 
+				System.out.println("Select one number between 1 - 5:");
+				num = input.nextInt();
+				if (num == 1) {
+					System.out.print("Stock Number:");
+					stock.number = input.nextInt();
+				}
+				else if (num == 2) {
+					System.out.print("Stock Name:");
+					stock.name = input.nextLine();
+				}
+				else if (num == 3) {
+					System.out.print("Stock Manufacturing Date:");
+					stock.mdate = input.nextLine();
+				}
+				else if (num == 4) {
+					System.out.print("Stock Expiration Date:");
+					stock.edate = input.nextLine();
+				}
+				else {
+					continue;
+				}
+			}
 		}
 	}
 	
