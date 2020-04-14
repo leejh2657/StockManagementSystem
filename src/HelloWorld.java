53f00fc90e8417782fbe00b2ba92f02c72f6824d
82102 <82102@DESKTOP-55UIN0I>
2020-04-14 14:24:17 +0900

    create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("*** Stock Management System Menu ***"); 
+		System.out.println(" 1. Add Stock");
+		System.out.println(" 2. Delete Stock"); 
+		System.out.println(" 3. Edit Stock"); 
+		System.out.println(" 4. View Stock"); 
+		System.out.println(" 5. Exit"); 
+		System.out.println("Select one number between 1 - 5"); 
+
+	}
+
+}
