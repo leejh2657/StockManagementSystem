c11351770c067f49583636455f154380c06b2a48
82102 <82102@DESKTOP-55UIN0I>
2020-04-28 10:32:58 +0900

    중간

---
+++ src/stock/FisheryStock.java
@@ -0,0 +1,58 @@
+package stock;
+
+import java.util.Scanner;
+
+public class FisheryStock extends Stock {
+	
+	protected String origin;
+	
+	public FisheryStock (StockKind Kind) {
+		this.kind = kind;
+	}
+	
+	public void getUserInput(Scanner input) {
+		System.out.print("Stock Number:");
+		int number = input.nextInt();
+		this.setNumber(number);
+		
+		System.out.print("Stock Name:");
+		String name = input.next();
+		this.setName(name);
+		
+		System.out.print("Stock Catching Date:");
+		String mdate = input.next();
+		this.setMdate(mdate);
+		
+		System.out.print("Stock Expiration Date:");
+		String edate = input.next();
+		this.setEdate(edate);
+		
+		System.out.print("The Country of Origin:");
+		String origin = input.next();
+		this.setEdate(origin);
+		
+		
+	}
+	public void printInfo() {
+		String skind = "none";
+		switch(this.kind) {
+		case Industrial:
+			skind = "Ind";
+			break;
+		case Agriculture:
+			skind = "Agri";
+			break;
+		case Fishery:
+			skind = "Fish";
+			break;
+		case Meat:
+			skind = "meat";
+			break;
+		default:
+			
+		}
+		System.out.println("kind:" + skind + "number:" + this.number + "name:" + this.name + "catching date:" + this.mdate + "expiration date:" + this.edate + "the country of origin:" + this.origin);
+		
+	}
+
+}
